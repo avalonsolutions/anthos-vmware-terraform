3be7452f49b06983fa490b4a8bae7f3ada0b0eb6
Emil Engfors <emil.engfors@devoteam.com>
2020-08-18 16:58:03 +0200

    created additional test vm

--- testvms.tf
+++ testvms.tf
@@ -1,5 +1,5 @@
 /*
-resource "vsphere_virtual_machine" "testvm1" {
+resource "vsphere_virtual_machine" "user-cluster_vm" {
   name             = "testvm1"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore1.id
@@ -22,7 +22,7 @@ resource "vsphere_virtual_machine" "testvm1" {
     template_uuid = data.vsphere_virtual_machine.template.id
   }
 }
-resource "vsphere_virtual_machine" "testvm2" {
+resource "vsphere_virtual_machine" "admin-cluster_vm" {
   name             = "testvm2"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore1.id
@@ -46,3 +46,26 @@ resource "vsphere_virtual_machine" "testvm2" {
   }
 }
 */
+resource "vsphere_virtual_machine" "migrate_vm" {
+  name             = "migrate-instance"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore1.id
+  host_system_id   = data.vsphere_host.esxi_host.id
+
+  num_cpus = 1
+  memory   = 2048
+  guest_id = "ubuntu64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.vm_network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 16
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+}
