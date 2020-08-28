deed14b73a8cf79a1181eb1d9ed85ff610f69951
Emil Engfors <emil.engfors@devoteam.com>
2020-08-28 15:12:13 +0200

    Consolidated files
    moved resources into main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "remote" {
-    organization = "DevoteamCloudServices"
-
-    workspaces {
-      name = "anthos-vmware-terraform-core"
-    }
-  }
-}
-
 resource "vsphere_host_port_group" "pg_admin" {
   name                = "Admin Network"
   host_system_id      = data.vsphere_host.esxi_host.id
@@ -27,6 +17,112 @@ resource "vsphere_host_port_group" "pg_user" {
 
   allow_promiscuous = true
 }
+
+resource "vsphere_virtual_machine" "ivm_vm" {
+  name             = var.bastion_host_name
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore1.id
+  host_system_id   = data.vsphere_host.esxi_host.id
+
+  num_cpus = 1
+  memory   = 2048
+  guest_id = "ubuntu64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.vm_network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 16
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+
+  provisioner "file" {
+    source      = "${var.script_path}/installvm.sh"
+    destination = "/tmp/installscript.sh"
+  }
+
+  provisioner "file" {
+    source      = "${var.sa_path}/whitelisted-key.json"
+    destination = "/home/${var.linux_user}/whitelisted-key.json"
+  }
+
+  provisioner "file" {
+    source      = "${var.sa_path}/gcp_account.json"
+    destination = "/home/${var.linux_user}/gcp_account.json"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/installscript.sh",
+      "cd /tmp && echo ${var.user_password} | sudo -S ./installscript.sh ${var.google_project} ${var.vsphere_user} ${var.vsphere_password} ${var.vsphere_server} ${var.linux_user}",
+    ]
+  }
+
+  connection {
+    type     = "ssh"
+    user     = var.linux_user
+    password = var.user_password
+    host     = self.guest_ip_addresses[0]
+  }
+}
+
+resource "vsphere_virtual_machine" "nws_vm" {
+  name             = var.networkservices_host_name
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore1.id
+  host_system_id   = data.vsphere_host.esxi_host.id
+
+  num_cpus = 1
+  memory   = 2048
+  guest_id = "ubuntu64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.vm_network.id
+  }
+
+  network_interface {
+    network_id = data.vsphere_network.admin_network.id
+  }
+
+  network_interface {
+    network_id = data.vsphere_network.user_network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 16
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+
+  provisioner "file" {
+    source      = "${var.script_path}/networkservices.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  // Script will fail if there's not 3 interfaces on the VM
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "cd /tmp && echo ${var.user_password} | sudo -S ./script.sh",
+    ]
+  }
+
+  connection {
+    type     = "ssh"
+    user     = var.linux_user
+    password = var.user_password
+    host     = self.guest_ip_addresses[0]
+  }
+}
+
 
 data "vsphere_datastore" "datastore1" {
   name          = "datastore1"
